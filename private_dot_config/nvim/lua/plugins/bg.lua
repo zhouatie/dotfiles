9821024fd89f20f79921f1e80d7e141f8d0422a6
zhoushitie <zhoushitie@corp.netease.com>
2024-09-16 13:35:10 +0800

    feat: 更新内容

---
+++ private_dot_config/nvim/lua/plugins/bg.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   opts = {
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
+  --   },
+  -- },
+}
