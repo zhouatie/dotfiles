7fd890a9d753b22289c02f50dd3c7fd549722f95
zhouatie <zhouatie@gmail.com>
2024-10-04 15:00:32 +0800

    fix: 删除一些不需要的文件

--- private_dot_config/nvim/lua/plugins/prettier.lua
+++ private_dot_config/nvim/lua/plugins/prettier.lua
@@ -9,7 +9,7 @@ return {
         ["javascriptreact"] = { "eslint_d" },
         ["typescript"] = { "eslint_d" },
         ["typescriptreact"] = { "eslint_d" },
-        ["vue"] = { "prettier" },
+        ["vue"] = { "eslint_d" },
         ["css"] = { "prettier", "stylelint" },
         ["scss"] = { "prettier", "stylelint" },
         ["less"] = { "prettier", "stylelint" },
